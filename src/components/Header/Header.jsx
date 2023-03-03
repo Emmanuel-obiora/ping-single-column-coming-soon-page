e2d1f0cba44aee8dca3e87d079389f7935b8b5c0
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-03-03 23:31:22 +0100

    Adding frontend mentor's template and creating react app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+         We are launching soon!
+
+Subscribe and get notified
+
+Notify Me
+        </header>
+    )
+}
+
+export default Header

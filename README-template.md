d971dbb7aa2a858d9afde3299c4e8c8ec1536047
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-03-04 20:47:55 +0100

    Adding web site interaction

--- README-template.md
+++ README-template.md
@@ -102,9 +102,9 @@ Use this section to outline areas that you want to continue focusing on in futur
 
 ## Author
 
-- Website - [Add your name here](https://www.your-site.com)
-- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
-- Twitter - [@yourusername](https://www.twitter.com/yourusername)
+- Website - [Obiora Emmanuel](https://emmanuel-obiora.github.io/portfolio-about-me-/web-content)
+- Frontend Mentor - [@Emmanuel-obiora](https://www.frontendmentor.io/profile/Emmanuel-obiora)
+- Twitter - [@Miroclesdgenius](https://twitter.com/Miroclesdgenius)
 
 **Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,52 @@ import './Header.css'
 import logo from '../../images/logo.svg'
 
 const Header = () => {
+
+    const sendNotification = (e) => {
+        e.preventDefault();
+
+        const email = document.getElementById("Email").value;
+        // console.log(email);
+
+        if (email === ""){
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.add('error-input');
+
+            const chgText = document.getElementById('chgText');
+            chgText.innerText = "Whoops! It looks like you forgot to add your email";
+        } 
+        else if (!isValid(email)){
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.add('error-input');
+
+            const chgText = document.getElementById('chgText');
+            chgText.innerText = "Please enter a valid email address";
+        } 
+        else {
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.remove('error-input');
+
+            alert(`A mail has been forwarded to ${email}`);
+        }
+    };
+
+    function isValid(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
     return (
         <header>
             <img src={logo} alt="Logo" />
             <h1>We are launching <span className='lnch'>soon!</span></h1>
             <small>Subscribe and get notified</small>
-            <div className="mail-visitor">
-                <input type="text" placeholder='Your email address' className="user-mail" />
-                <button className="notify-btn">Notify Me</button>
-            </div>
+            <form className="mail-visitor" onSubmit={sendNotification}>
+                <div className="error">
+                    <input type="text" id='Email' placeholder='Your email address' className="user-mail" />
+                    <span id='chgText' className="error-message"></span>
+                </div>
+                <button type="submit" className="notify-btn" id="notify" >Notify Me</button>
+            </form>
         </header>
     )
 }
